d093695b8715b782c02c996c94184bd62818c39e
princeassasin <sriramlakshmim@gmail.com>
2017-07-04 16:10:58 +0530

    modified Cassandra_Keyspace.java and added CassandraQuries.java

--- src/com/cassandrakeyspace/Cassadra_Keyspace.java
+++ src/com/cassandrakeyspace/Cassadra_Keyspace.java
@@ -1,28 +1,17 @@
 package com.cassandrakeyspace;
 
-//import com.datastax.*;
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
+public abstract class Cassadra_Keyspace  {
 
-public class Cassadra_Keyspace {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		  String query = "CREATE KEYSPACE tp WITH replication "
-			         + "= {'class':'SimpleStrategy', 'replication_factor':3};";
-			                    
-			      //creating Cluster object
-			      Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-			    
-			      //Creating Session object
-			      Session session = cluster.connect();
-			     
-			      //Executing the query
-			      session.execute(query);
-			     
-			      //using the KeySpace
-			      session.execute("USE tp");
-			      System.out.println("Keyspace created"); 
+	public String createquery() {
+	
+		 String query = "CREATE KEYSPACE sriram WITH replication "
+		         + "= {'class':'SimpleStrategy', 'replication_factor':3};";		    
+	
+		 return query;
+		 
+		
 	}
-
+	
+	public abstract String  createquery2(); 
+	
 }
